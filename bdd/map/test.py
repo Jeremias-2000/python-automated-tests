ef7109111bc0da686009f64dcc39700f9c1ff4bc
Jeremias-2000 <jdossantospinheiro20@gmail.com>
2024-04-04 10:17:56 -0300

    ajuste forma pegar file path

--- bdd/map/test.py
+++ bdd/map/test.py
@@ -1,9 +1,17 @@
 from csv import reader
+import os
 
+def find_project_file(filename):
+    current_dir = os.getcwd()
+    data_folder = os.path.join(current_dir, 'bdd', 'data')
+    if os.path.exists(data_folder):
+        return f'{data_folder}/{filename}'
+    else:
+        raise FileNotFoundError(f"The file '{filename}' does not  not found within the 'bdd/data' folder or not found in the directory structure.")
 
 def ler_csv_cenario(massa:str,cenario:str):
-    caminho = '/home/jeremias/Documentos/workspace/test-python-integration2/bdd/data/' + massa + '.csv'
-    caminho = caminho.replace('"', '')
+    caminho = find_project_file(f'{massa}.csv')
+   
     print("File path:", caminho)  # Print file path for debugging
     try:
         with open(caminho) as csv:
@@ -14,7 +22,6 @@ def ler_csv_cenario(massa:str,cenario:str):
                         print(linha[2])
                         break
     except FileNotFoundError:
-        print(f"File '{caminho}' not found.")
-    
+        print(f"File '{caminho}' not found.")    
 
 ler_csv_cenario('test','CENARIO')        
